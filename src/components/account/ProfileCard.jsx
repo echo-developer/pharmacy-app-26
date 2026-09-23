58a406f72923e4423d7d42fdd31943095121e727
Ujjawal Kumar Pandey <ujjawal.originatesoft@gmail.com>
2026-09-23 16:14:17 +0530

    updated

--- src/components/account/ProfileCard.jsx
+++ src/components/account/ProfileCard.jsx
@@ -80,11 +80,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 4,
+    // elevation: 3,
   },
 });
 
